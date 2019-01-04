3b3f2041a18e7977b051736ad34864f1595e949d
KRUGER\ecordova <ecordova@kruger.com.ec>
2019-01-04 17:38:21 -0500

    configuracion de dashboard y detalles

---
+++ src/app/ingreso-egreso/ingreso-egreso.service.ts
@@ -0,0 +1,57 @@
+import {Injectable} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {IngresoEgresoModel} from './ingreso-egreso.model';
+import {AuthService} from '../auth/auth.service';
+import {Store} from '@ngrx/store';
+import {AppState} from '../app.reducer';
+import {filter, map} from 'rxjs/operators';
+import {SetItemsActions} from './ingreso-egreso.actions';
+import {Subscription} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IngresoEgresoService {
+  ingresoEgresoSubscriptions: Subscription = new Subscription();
+
+  constructor(private afDB: AngularFirestore, private authService: AuthService, private store: Store<AppState>) {
+  }
+
+  crearIngresoEgreso(ingresoEgreso: IngresoEgresoModel): Promise<any> {
+    const user = this.authService.getUser();
+    return this.afDB.doc(`${user.uid}/ingresos-egresos`).collection('items').add({...ingresoEgreso});
+  }
+
+  deleteIngresoEgreso(uid: String) {
+    const user = this.authService.getUser();
+    return this.afDB.doc(`${user.uid}/ingresos-egresos/items/${uid}`).delete();
+  }
+
+  initIngresoEgrsoListener() {
+    this.ingresoEgresoSubscriptions.add(this.store.select('auth')
+      .pipe(filter(auth => !!auth.user))
+      .subscribe(auth => {
+        this.ingresoEgresoItems(auth.user.uid);
+      }));
+  }
+
+  private ingresoEgresoItems(uid: String) {
+    this.ingresoEgresoSubscriptions.add(this.afDB.collection(`${uid}/ingresos-egresos/items`)
+      .snapshotChanges()
+      .pipe(map(snapshot => {
+          return snapshot.map(s => {
+            return {
+              uid: s.payload.doc.id, ...s.payload.doc.data()
+            };
+          });
+        })
+      )
+      .subscribe((collection: any) => {
+        this.store.dispatch(new SetItemsActions(collection));
+      }));
+  }
+
+  cancelSubscriptions() {
+    this.ingresoEgresoSubscriptions.unsubscribe();
+  }
+}
